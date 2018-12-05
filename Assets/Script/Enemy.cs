4491809c179ff0ed4301f1b3f367bd04f13fa2b8
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-12-05 14:55:08 -0500

    WIP Enemy et SpawnPoint

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    public GameObject Player;
+    int MoveSpeed = 4;
+    int MaxDist = 10;
+    int MinDist = 5;
+
 	// Use this for initialization
 	void Start () {
-		
+        this.Player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        transform.LookAt(Player.transform);
+
+        if (Vector3.Distance(transform.position, Player.transform.position) >= this.MinDist)
+        {
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+            if (Vector3.Distance(transform.position, Player.transform.position) <= this.MaxDist)
+            {
+                // Debug.Log("close to the player");
+            }
+        }
 		
 	}
 }
---
+++ Assets/Script/SpawnPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    float Timer;
+    int SecondsTimer;
+
+	// Use this for initialization
+	void Start () {
+        this.Timer = 0.0f;
+        this.SecondsTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.Timer += Time.deltaTime;
+
+        if (this.SecondsTimer != (int)this.Timer % 60)
+        {
+            this.SecondsTimer = (int)this.Timer % 60;
+            this.SpawnNextEnemy();
+        }
+
+	}
+
+    void SpawnNextEnemy()
+    {
+        //GameObject newEnemy = Instantiate(new Enemy().gameObject);
+        //newEnemy.transform.position = new Vector3(0, -1 + transform.localScale.y / 2, 0);
+
+        //GameObject newEnemy = GameObject.CreatePrimitive();
+        // Besoin de regarder de la doc sur les prefab
+
+    }
+}
