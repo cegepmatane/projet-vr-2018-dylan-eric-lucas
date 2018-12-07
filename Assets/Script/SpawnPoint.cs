daeeee5fc8c9765550ea1443f33446b8c580df93
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-12-07 11:00:42 -0500

    Changements spawning + random position autour du joueur

--- Assets/Script/SpawnPoint.cs
+++ Assets/Script/SpawnPoint.cs
@@ -4,30 +4,62 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour {
 
+    public GameObject Player;
     float Timer;
-    int SecondsTimer;
+    public float SpawnDelay = 2;
+    int Wave;
 
 	// Use this for initialization
-	void Start () {
-        this.Timer = 0.0f;
-        this.SecondsTimer = 0;
+	void Start ()
+    {
+        this.Player = GameObject.FindGameObjectWithTag("Player");
+        this.Wave = 1;
+
+        StartCoroutine(SpawnRoutine());
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        this.Timer += Time.deltaTime;
+	void Update ()
+    {
+        /*this.Timer += Time.deltaTime;
 
         if (this.SecondsTimer != (int)this.Timer % 60)
         {
             this.SecondsTimer = (int)this.Timer % 60;
-            this.SpawnNextEnemy();
-        }
+
+            float randomNumber = Random.Range(0.0f, 1.0f);
+            randomNumber += Mathf.Ceil(randomNumber * this.SecondsTimer / 100);
+            for (int i = 0; i <= (int)randomNumber; i++)
+            {
+                this.SpawnNextEnemy();
+            }
+        }*/
 
 	}
+
+    IEnumerator SpawnRoutine()
+    {
+        while(true)
+        {
+            this.SpawnNextEnemy();
+
+            yield return new WaitForSeconds(SpawnDelay);
+        }
+    }
 
     void SpawnNextEnemy()
     {
         GameObject newEnemy = Instantiate(Resources.Load("EnemyPrefab")) as GameObject;
-        newEnemy.transform.position = new Vector3(0, -1 + transform.localScale.y / 2, 0);
+        Vector3 newEnnemyPosition = getRandomPosition();
+        while(Vector3.Distance(Player.transform.position, newEnnemyPosition) < 15.0f)
+        {
+            newEnnemyPosition = getRandomPosition();
+        }
+        newEnemy.transform.position = newEnnemyPosition;
+    }
+
+    Vector3 getRandomPosition()
+    {
+        return new Vector3(Random.Range(-30.0f, 30.0f), 0.0f, Random.Range(-30.0f, 30.0f));
     }
 }
