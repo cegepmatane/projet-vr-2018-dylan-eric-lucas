ba74cf0e206d79045953159ea16cc2d4750e98c2
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-12-10 09:43:39 -0500

    Refactoring pour diviser le code et les responsabilités des classes

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    private static Score instance;
+
+    public int ScorePoints;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public static Score getInstance()
+    {
+        return instance;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.ScorePoints = 0;
+        StartCoroutine(ScoreUp());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    IEnumerator ScoreUp()
+    {
+        while (true)
+        {
+            this.ScorePoints++;
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
--- Assets/Script/SpawnPoint.cs
+++ Assets/Script/SpawnPoint.cs
@@ -1,34 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SpawnPoint : MonoBehaviour {
 
     public GameObject Player;
-    int Score;
     public float SpawnDelay = 2;
 
-    GameObject UIScore;
-    Text UITextComponent;
 
 	// Use this for initialization
 	void Start ()
     {
         this.Player = GameObject.FindGameObjectWithTag("Player");
-        this.Score = 0;
-
-        this.UIScore = GameObject.Find("UIScore");
-        this.UITextComponent = UIScore.GetComponent<Text>();
 
         StartCoroutine(SpawnRoutine());
-        StartCoroutine(ScoreUp());
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        this.UITextComponent.text = "Score : " + this.Score;
+        
 	}
 
     IEnumerator SpawnRoutine()
@@ -40,21 +31,12 @@ public class SpawnPoint : MonoBehaviour {
         }
     }
 
-    IEnumerator ScoreUp()
-    {
-        while (true)
-        {
-            this.Score++;
-            yield return new WaitForSeconds(1);
-        }
-    }
-
     void SpawnNextEnemy()
     {
         GameObject newEnemy = Instantiate(Resources.Load("EnemyPrefab")) as GameObject;
 
         Vector3 newEnnemyPosition = getRandomPosition();
-        while(Vector3.Distance(Player.transform.position, newEnnemyPosition) < 15.0f)
+        while (Vector3.Distance(Player.transform.position, newEnnemyPosition) < 20.0f)
         {
             newEnnemyPosition = getRandomPosition();
         }
